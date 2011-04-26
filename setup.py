570a79bd1a21a4c37f9bace352f9a544d2f06c26
Tomasz Wysocki <tomasz@wysocki.info>
2011-04-26 19:32:00 +0200

    Change facebook-javascript-authentication name.
    Change-Id: Ic56d8656b27264c0ec3492d20add442108548db4

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from distutils.core import setup
 
 setup(
-    name='FacebookJavaScriptAuthentication',
-    version='0.1',
+    name='facebook-javascript-authentication',
+    version='1.0',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
