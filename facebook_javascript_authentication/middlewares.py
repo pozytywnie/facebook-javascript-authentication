da2f128522d6ff2291281d27a40ad048e4a70ddc
Tomasz Wysocki <tomasz@wysocki.info>
2011-09-29 15:37:24 +0200

    Add P3PMiddleware.
    Change-Id: I4a51168deab8ce2d65ec255593fa7d42e383d3cb

---
+++ facebook_javascript_authentication/middlewares.py
@@ -0,0 +1,9 @@
+class P3PMiddleware():
+    def process_request(self, request):
+        if not request.session.items():
+            request.session.modified = True # force create session
+
+    def process_response(self, request, response):
+        request.session['foo'] = 'bar'
+        response['P3P'] = 'CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"'
+        return response
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='facebook-javascript-authentication',
-    version='1.1',
+    version='1.2',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
