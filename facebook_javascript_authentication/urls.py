4b67a6f056120df215aa78a99330d32989604472
Piotr Maliński <piotr@pozytywnie.pl>
2013-01-09 13:02:38 +0100

    Remove deprecated import path.
    Change-Id: I8f78301b1a4e76d2c250f8602643105a71a3320e

--- facebook_javascript_authentication/urls.py
+++ facebook_javascript_authentication/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls.defaults import patterns
-from django.conf.urls.defaults import url
+from django.conf.urls import patterns
+from django.conf.urls import url
 from django.core.urlresolvers import reverse
 
 urlpatterns = patterns('facebook_javascript_authentication.views',
