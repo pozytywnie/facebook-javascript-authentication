92f99b7eb916c232d3b2de0a7d84cbba3bbac22c
Piotr Maliński <piotr@pozytywnie.pl>
2014-04-17 12:04:19 +0200

    Convert function view to class-based view.
    Change-Id: Idbd984772cd2752910fc8e18e2b12faba7d4f046

---
+++ facebook_javascript_authentication/tests.py
@@ -0,0 +1,43 @@
+import json
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import RequestFactory
+from django.test import TestCase
+
+from facebook_javascript_authentication import views
+
+
+class AuthenticateFacebookUserTest(TestCase):
+    @mock.patch('facebook_auth.utils.get_from_graph_api')
+    def test_if_user_is_authenticated(self, graph_api_mock):
+        factory = RequestFactory()
+        request = factory.post('/')
+        session = SessionMiddleware()
+        session.process_request(request)
+        request.POST = {
+            'access_token': 'aaa',
+            'token_expires_in': 100
+        }
+        data_dict = {'email': 'aaa@aaa.com', 'name': 'Aaa',
+                     'last_name': 'Zzz',
+                     'id': '740277901',
+                     'username': 'aaaa',
+                     'link': 'https://www.facebook.com/aaaa',
+                     'gender': 'male',
+                     'locale': 'pl_PL',
+                     'first_name': 'Asss',
+                     'verified': True, 'timezone': 2}
+        graph_api_mock.return_value = data_dict
+
+        response = views.authenticate(request)
+        self.assertEqual(200, response.status_code)
+        response_data = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(data_dict['last_name'], response_data['last_name'])
+        self.assertEqual(data_dict['email'], response_data['email'])
+        self.assertEqual(data_dict['id'], response_data['username'])
+        self.assertEqual(data_dict['first_name'], response_data['first_name'])
+        self.assertEqual('ok', response_data['status'])
--- facebook_javascript_authentication/views.py
+++ facebook_javascript_authentication/views.py
@@ -9,43 +9,46 @@ from django.utils.encoding import force_text
 from django.utils import timezone
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
 logger = logging.getLogger(__name__)
 
 FALLBACK_EXPIRES_IN_SECONDS = 900
 
-@never_cache
-@csrf_exempt
-def authenticate(request, authenticate=auth.authenticate, login=auth.login):
-    access_token = request.POST.get('access_token', None)
-    token_expiration_date = _get_token_expiration_date(request)
-    if access_token != None:
-        user = authenticate(access_token=access_token,
-                            token_expiration_date=token_expiration_date)
-        if user is not None:
-            login(request, user)
-            data = json.dumps({'status': 'ok',
-                               'csrf_token': force_text(csrf(request).get('csrf_token', None)),
-                               'user_id': user.id,
-                               'first_name': user.first_name,
-                               'last_name': user.last_name,
-                               'email': user.email,
-                               'username': user.username})
-            return HttpResponse(data, content_type='application/json')
-    return HttpResponse(json.dumps({'status': 'error'}), content_type='application/json')
 
+class AuthenticateFacebookUser(View):
+    def post(self, request, authenticate=auth.authenticate, login=auth.login, *args, **kwargs):
+        access_token = request.POST.get('access_token', None)
+        token_expiration_date = self._get_token_expiration_date(request)
+        if access_token != None:
+            user = authenticate(access_token=access_token,
+                                token_expiration_date=token_expiration_date)
+            if user is not None:
+                login(request, user)
+                data = json.dumps({'status': 'ok',
+                                   'csrf_token': force_text(csrf(request).get('csrf_token', None)),
+                                   'user_id': user.id,
+                                   'first_name': user.first_name,
+                                   'last_name': user.last_name,
+                                   'email': user.email,
+                                   'username': user.username})
+                return HttpResponse(data, content_type='application/json')
+        return HttpResponse(json.dumps({'status': 'error'}), content_type='application/json')
 
-def _get_token_expiration_date(request):
-    token_expiration_date = None
-    if 'token_expires_in' in request.POST:
-        token_expires_in = request.POST['token_expires_in']
-        try:
-            expires_in_seconds = int(token_expires_in)
-        except ValueError:
-            logger.warning('Invalid token_expires_in',
-                           extra={'token_expires_in': token_expires_in,
-                                  'request': request})
-            expires_in_seconds = FALLBACK_EXPIRES_IN_SECONDS
-        expires_in = datetime.timedelta(seconds=expires_in_seconds)
-        token_expiration_date = timezone.now() + expires_in
-    return token_expiration_date
+    @staticmethod
+    def _get_token_expiration_date(request):
+        token_expiration_date = None
+        if 'token_expires_in' in request.POST:
+            token_expires_in = request.POST['token_expires_in']
+            try:
+                expires_in_seconds = int(token_expires_in)
+            except ValueError:
+                logger.warning('Invalid token_expires_in',
+                               extra={'token_expires_in': token_expires_in,
+                                      'request': request})
+                expires_in_seconds = FALLBACK_EXPIRES_IN_SECONDS
+            expires_in = datetime.timedelta(seconds=expires_in_seconds)
+            token_expiration_date = timezone.now() + expires_in
+        return token_expiration_date
+
+authenticate = csrf_exempt(never_cache(AuthenticateFacebookUser.as_view()))
