52ae3846d74531b979ba7d4b18604a273c6a6769
Tomasz Pazurkiewicz <tomasz.pazurkiewicz@pozytywnie.pl>
2014-08-29 15:06:26 +0200

    Bump version.
    Change-Id: I4b13c3e55c1d019372894ebc7d9781d710f06d04

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='facebook-javascript-authentication',
-    version='3.0.8',
+    version='3.0.9',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
