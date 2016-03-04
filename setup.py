f738b93e649c4d0071bd2d1f9e21eb906a7a5fad
Jakub Skiepko <jakub@pozytywnie.pl>
2016-03-04 16:16:07 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='facebook-javascript-authentication',
-    version='3.1.0',
+    version='3.2.0',
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
     install_requires=(
-        'django',
+        'django>=1.8',
         'django-facebook-auth>=2.7.4',
         'django-javascript-settings',
     ),
